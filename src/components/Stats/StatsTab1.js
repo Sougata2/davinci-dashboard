31a96e9f1b56d762b684208760c04704e67f5858
sougata <sougata19582001@gmail.com>
2024-08-22 18:11:56 +0530

    images added

--- src/components/Stats/StatsTab1.js
+++ src/components/Stats/StatsTab1.js
@@ -1,9 +1,14 @@
-function StatsTab1({ title, data, attribute }) {
+function StatsTab1({ title, image, data, attribute }) {
   return (
     <div className="stats-data">
-      <div className="stats-data-title-1">{title}</div>
+      <div className="stats-image-title">
+        <div className="stats-image">
+          <img src={image} alt={title} />
+        </div>
+        <div className="stats-data-title-1">{title}</div>
+      </div>
       <div className="stats-data-stat-1">
-        {data.toLocaleString()}{" "}
+        {data.toLocaleString()}&nbsp;
         <span className="stats-data-attribute-1">{attribute}</span>
       </div>
     </div>
--- src/components/Stats/StatsTab2.js
+++ src/components/Stats/StatsTab2.js
@@ -1,12 +1,12 @@
-function StatsTab2({ title, data, attribute }) {
+function StatsTab2({ title, image, data, attribute }) {
   return (
     <div className="stats-data tab-2">
       <div className="stats-data-title-1">{title}</div>
-      <div >
-        <img src="" alt="" className="" />
+      <div className="stat-image">
+        <img src={image} alt="" className="" />
       </div>
       <div className="stats-data-stat-1">
-        {data.toLocaleString()}{" "}
+        {data.toLocaleString()}&nbsp;
         <span className="stats-data-attribute-1">{attribute}</span>
       </div>
     </div>
