f9f4d60322932b1e99903e72b3174d14e2e7c4e1
sougata <sougata19582001@gmail.com>
2024-08-22 21:32:20 +0530

    finished

--- src/components/Graph/BarGraph.js
+++ src/components/Graph/BarGraph.js
@@ -1,30 +1,38 @@
-import React from 'react';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import React from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
 
 const data = [
-  { name: '1', value: 200000 },
-  { name: '2', value: 50000 },
-  { name: '3', value: 100000 },
-  { name: '4', value: 150000 },
-  { name: '5', value: 100000 },
-  { name: '6', value: 50000 },
-  { name: '7', value: 10000 },
-  { name: '8', value: 50000 },
-  { name: '9', value: 100000 },
-  { name: '10', value: 120000 },
-  { name: '11', value: 150000 },
-  { name: '12', value: 180000 },
+  { name: "1", value: 200000 },
+  { name: "2", value: 50000 },
+  { name: "3", value: 100000 },
+  { name: "4", value: 150000 },
+  { name: "5", value: 100000 },
+  { name: "6", value: 50000 },
+  { name: "7", value: 10000 },
+  { name: "8", value: 50000 },
+  { name: "9", value: 100000 },
+  { name: "10", value: 120000 },
+  { name: "11", value: 150000 },
+  { name: "12", value: 0 },
 ];
 
 const SimpleBarChart = () => {
   return (
-    <BarChart width={800} height={300} data={data}>
+    <BarChart width={1100} height={300} data={data}>
       <CartesianGrid strokeDasharray="3 3" />
       <XAxis dataKey="name" />
       <YAxis />
       <Tooltip />
       <Legend />
-      <Bar dataKey="value" fill="teal" />
+      <Bar dataKey="value" fill="teal" barSize={20} radius={[10, 10, 0, 0]}/>
     </BarChart>
   );
 };
