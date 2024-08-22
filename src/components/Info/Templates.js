a84ac551acace61dca5175288b6f40aed902462e
sougata <sougata19582001@gmail.com>
2024-08-22 21:52:03 +0530

    changed the image path

--- src/components/Info/Templates.js
+++ src/components/Info/Templates.js
@@ -4,32 +4,32 @@ import Template from "./Template";
 const templates = [
   {
     title: "Taking Points",
-    image: "/images/taking-points.png",
+    image: "images/taking-points.png",
     desc: "Write short simple and informative points for the subheadings of your articles",
   },
   {
     title: "Startup Name Generator",
-    image: "/images/startup-name-generator.png",
+    image: "images/startup-name-generator.png",
     desc: "Generate cool, creative and catchy names for your statup in seconds",
   },
   {
     title: "Pros and Cons",
-    image: "/images/pros-cons.png",
+    image: "images/pros-cons.png",
     desc: "write Pros and Cons of product, service or website for your blog article",
   },
   {
     title: "Product Name Generator",
-    image: "/images/product-name-generator.png",
+    image: "images/product-name-generator.png",
     desc: "Generate paragraphs about any topic including a keyword and in a specific tone of voice",
   },
   {
     title: "Product Description",
-    image: "/images/product-description.png",
+    image: "images/product-description.png",
     desc: "Write the description about your product and why it worth it",
   },
   {
     title: "Article Generator",
-    image: "/images/article-generator.png",
+    image: "images/article-generator.png",
     desc: "Turn a title and outline text into a fully completed high quality article within seconds",
   },
 ];
--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -7,17 +7,17 @@ function Stats() {
       <div className="stats-title">Major Stats</div>
       <div className="stats-description">This is what we have got for you today.</div>
       <div className="stats-row-2">
-        <StatsTab1 title={"Words Left"} data={5496893} image={"/images/words-left.png"} />
-        <StatsTab1 title={"Images Left"} data={5508229} image={"/images/images-left.png"} />
+        <StatsTab1 title={"Words Left"} data={5496893} image={"images/words-left.png"} />
+        <StatsTab1 title={"Images Left"} data={5508229} image={"images/images-left.png"} />
       </div>
       <div className="stats-row-3">
-        <StatsTab1 title={"Characters Left"} data={949638552} image={"/images/characters-left.png"} />
-        <StatsTab1 title={"Minutest Left"} data={189871} image={"/images/minutes-left.png"}/>
+        <StatsTab1 title={"Characters Left"} data={949638552} image={"images/characters-left.png"} />
+        <StatsTab1 title={"Minutest Left"} data={189871} image={"images/minutes-left.png"}/>
         <StatsTab1
           title={"Documents Created"}
           data={2266}
           attribute={"contents"}
-          image={"/images/documents-created.png"}
+          image={"images/documents-created.png"}
         />
       </div>
       <div className="stats-row-5">
@@ -25,16 +25,16 @@ function Stats() {
           title={"Wrods generated"}
           data={425930}
           attribute={"words"}
-          image={"/images/words-generated.png"}
+          image={"images/words-generated.png"}
         />
-        <StatsTab2 title={"Images created"} data={0} attribute={"Images"} image={"/images/images-created.png"} />
-        <StatsTab2 title={"Codes generated"} data={256} attribute={"Codes"} image={"/images/codes-generated.png"} />
-        <StatsTab2 title={"voiceOver Tasks"} data={455} attribute={"words"} image={"/images/voiceover-tasks.png"}/>
+        <StatsTab2 title={"Images created"} data={0} attribute={"Images"} image={"images/images-created.png"} />
+        <StatsTab2 title={"Codes generated"} data={256} attribute={"Codes"} image={"images/codes-generated.png"} />
+        <StatsTab2 title={"voiceOver Tasks"} data={455} attribute={"words"} image={"images/voiceover-tasks.png"}/>
         <StatsTab2
           title={"Audio Transcribed"}
           data={2}
           attribute={"audio files"}
-          image={"/images/audio-transcribed.png"}
+          image={"images/audio-transcribed.png"}
         />
       </div>
     </div>
